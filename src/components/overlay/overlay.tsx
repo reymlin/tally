74d09ed62ab022238a8ed3ea6a12e6cbc9f293e6
lml <1395531765@qq.com>
2023-04-16 09:58:31 +0800

    4-16 完善浮层样式

--- src/components/overlay/overlay.tsx
+++ src/components/overlay/overlay.tsx
@@ -1,5 +1,9 @@
 import { defineComponent, PropType } from "vue";
 import S from "./overlay.module.scss";
+import chartImg from "@/assets/imgs/chart.png";
+import exportImg from "@/assets/imgs/export.png";
+import fenleiImg from "@/assets/imgs/fenlei.png";
+import tixingImg from "@/assets/imgs/tixing.png";
 export const Overlay = defineComponent({
     props: {
         onClose: {
@@ -16,25 +20,33 @@ export const Overlay = defineComponent({
         return () => (
             <>
                 <div class={S.leftbox}>
-                    <section>
-                        <p>未登录用户</p>
-                        <p>点击这里登录</p>
+                    <section class={S.overlayTop}>
+                        <p class={S.userName}>未登录用户</p>
+                        <p class={S.subtitle}>点击这里登录</p>
                     </section>
-                    <section>
+                    <section class={S.navList}>
                         <div>
-                            <p></p>
+                            <p>
+                                <img src={chartImg} alt="" />
+                            </p>
                             <span>统计图表</span>
                         </div>
                         <div>
-                            <p></p>
+                            <p>
+                                <img src={exportImg} alt="" />
+                            </p>
                             <span>导出数据</span>
                         </div>
                         <div>
-                            <p></p>
+                            <p>
+                                <img src={fenleiImg} alt="" />
+                            </p>
                             <span>自定义分类</span>
                         </div>
                         <div>
-                            <p></p>
+                            <p>
+                                <img src={tixingImg} alt="" />
+                            </p>
                             <span>记账提醒</span>
                         </div>
                     </section>
