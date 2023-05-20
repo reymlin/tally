de2ec85711f58672c6ba1fdbd88a4c0c3ad5fac9
lml <1395531765@qq.com>
2023-05-20 14:14:28 +0800

    5-20 update

--- src/views/create-note/index.tsx
+++ src/views/create-note/index.tsx
@@ -1,18 +1,27 @@
 import { defineComponent, ref } from "vue";
 import { MianLayout } from "@/components/mainLayout/mainLayout";
 import returnImg from "@/assets/imgs/return.png";
+import { useRouter } from "vue-router";
 export const CreateNote = defineComponent({
     setup(props, context) {
+        const router = useRouter();
+
+        const tabs = [{ name: "支出" }, { name: "收入" }];
+
         const selectedValue = ref("支出");
+
+        const onBack = () => {
+            router.push("/start");
+        };
+
         const changeSelectedValue = (value: string) => {
             selectedValue.value = value;
         };
-        const tabs = [{ name: "支出" }, { name: "收入" }];
         return () => (
             <>
                 <MianLayout v-model:selectedValue={selectedValue.value} v-model:changeSelectedValue={changeSelectedValue} v-model:tabs={tabs}>
                     {{
-                        leftImg: () => <img src={returnImg} />,
+                        leftImg: () => <img src={returnImg} onClick={onBack} />,
                         title: () => "记一笔",
                         main: () => (selectedValue.value === "支出" ? <div>支出</div> : <div>收入</div>)
                     }}
